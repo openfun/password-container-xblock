ff8f09943479c1ceba8362a99c7b484f5f1b94df
Oksana Slusarenro <oksana.slu@gmail.com>
2016-01-11 14:47:49 +0200

    Replace ugettext_lazy on _.

--- password_container/forms.py
+++ password_container/forms.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import ugettext_lazy as _
 
 
 class PasswordContainerXBlockForm(forms.Form):
-    group_id = forms.CharField(label=ugettext_lazy(u"Group ID"),
-            help_text=ugettext_lazy(u"All activities with this group ID will be unlocked at the same time."))
-    start_date = forms.SplitDateTimeField(label=ugettext_lazy(u"Start visibility"),
-            help_text=ugettext_lazy(u"Start date (MM/DD/YYYY) / time (HH/MM/SS, using 24 hour clock) for student to enter the password. Set time in EST the software will show it in UTC"))
-    end_date = forms.SplitDateTimeField(label=ugettext_lazy(u"End Visibility"),
-            help_text=ugettext_lazy(u"End date (MM/DD/YYYY) / time (HH/MM/SS, using 24 hour clock) for student to enter the password. Set time in EST - the software will show it in UTC"))
-    password = forms.CharField(label=ugettext_lazy(u"Password"),
-            help_text=ugettext_lazy(u"Password to start the activities"))
-    duration = forms.IntegerField(label=ugettext_lazy(u"Assessment duration"),
-            help_text=ugettext_lazy(u"Time to complete the ativities once the student has entered the password (in muinutes)"))
+    group_id = forms.CharField(label=_(u"Group ID"),
+            help_text=_(u"All activities with this group ID will be unlocked at the same time."))
+    start_date = forms.SplitDateTimeField(label=_(u"Start visibility"),
+            help_text=_(u"Start date (MM/DD/YYYY) / time (HH/MM/SS, using 24 hour clock) for student to enter the password. Set time in EST the software will show it in UTC"))
+    end_date = forms.SplitDateTimeField(label=_(u"End Visibility"),
+            help_text=_(u"End date (MM/DD/YYYY) / time (HH/MM/SS, using 24 hour clock) for student to enter the password. Set time in EST - the software will show it in UTC"))
+    password = forms.CharField(label=_(u"Password"),
+            help_text=_(u"Password to start the activities"))
+    duration = forms.IntegerField(label=_(u"Assessment duration"),
+            help_text=_(u"Time to complete the ativities once the student has entered the password (in muinutes)"))
--- password_container/models.py
+++ password_container/models.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import ugettext_lazy as _
 
 from xmodule_django.models import CourseKeyField
 
 
 class GroupConfiguration(models.Model):
     course_id = CourseKeyField(max_length=255, db_index=True,
-            verbose_name=ugettext_lazy(u"Course"))
-    group_id = models.CharField(max_length=100, verbose_name=ugettext_lazy(u"Group ID"))
-    start_date = models.DateTimeField(verbose_name=ugettext_lazy(u"Start visibility",))
-    end_date = models.DateTimeField(verbose_name=ugettext_lazy(u"End Visibility",))
-    password = models.CharField(max_length=100, verbose_name=ugettext_lazy(u"Password"))
-    duration = models.PositiveIntegerField(verbose_name=ugettext_lazy(u"Assessment duration"))
+            verbose_name=_(u"Course"))
+    group_id = models.CharField(max_length=100, verbose_name=_(u"Group ID"))
+    start_date = models.DateTimeField(verbose_name=_(u"Start visibility",))
+    end_date = models.DateTimeField(verbose_name=_(u"End Visibility",))
+    password = models.CharField(max_length=100, verbose_name=_(u"Password"))
+    duration = models.PositiveIntegerField(verbose_name=_(u"Assessment duration"))
 
     class Meta:
         verbose_name = "Groupe de xblocks"
